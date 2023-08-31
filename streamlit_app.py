57f0596422c51e3e0d9bbb9f022bf51d1b0fedaf
Anotherculms <143687864+Anotherculms@users.noreply.github.com>
2023-08-31 19:00:09 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,4 +47,8 @@ my_cur.execute( "select * from pc_rivery_db.public.fruit_load_list")
 my_data_row = my_cur.fetchone()
 streamlit.text(" The fruit list contains: ")
 streamlit.dataframe(my_data_row)
+
+fruit_choice2 = streamlit.text_input('What fruit would you like ?','Kiwi')
+insert into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST values('test')
+my_cur.execute( "insert into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST values('from streamlit')")
 streamlit.header('Breakfast 000 END')
