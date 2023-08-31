8126e9134e0d3ef5eedb00556ce30ec9f870123b
Anotherculms <143687864+Anotherculms@users.noreply.github.com>
2023-08-31 11:46:42 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,6 +17,9 @@ import pandas as pd
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
 
+my_fruit_list = my_fruit_list.set_index('Fruit')
+
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
 
 streamlit.header('Breakfast END')
